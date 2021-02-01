c71ab8e9c900d3f451302552cc110e35fb67f7f7
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-02-01 11:32:46 -0500

    Toughness and Knockback Resistance are working properly now, all configurable

--- src/main/java/chronosacaria/mcsa/bases/ArmorItemBase.java
+++ src/main/java/chronosacaria/mcsa/bases/ArmorItemBase.java
@@ -1,7 +1,12 @@
 package chronosacaria.mcsa.bases;
 
 import chronosacaria.mcsa.Mcsa;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.util.Identifier;
@@ -16,9 +21,32 @@ public class ArmorItemBase extends ArmorItem {
             UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"),
             UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
 
+
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+
     public ArmorItemBase(ArmorMaterial armorMaterial, EquipmentSlot slot, Settings settings,
                                String id){
         super(armorMaterial, slot, settings);
+
+        int protection = armorMaterial.getProtectionAmount(slot);
+        float toughness = armorMaterial.getToughness();
+
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        UUID uuid = ARMOR_MODIFIERS[slot.getEntitySlotId()];
+        builder.put(EntityAttributes.GENERIC_ARMOR, new EntityAttributeModifier(uuid, "Armor modifier",
+                protection, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, new EntityAttributeModifier(uuid, "Armor toughness",
+                toughness, EntityAttributeModifier.Operation.ADDITION));
+        if(this.knockbackResistance > 0) {
+            builder.put(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, new EntityAttributeModifier(uuid, "Armor knockback resistance",
+                    this.knockbackResistance, EntityAttributeModifier.Operation.ADDITION));
+        }
+        this.attributeModifiers = builder.build();
         Registry.register(Registry.ITEM, new Identifier(Mcsa.MOD_ID, id), this);
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot){
+        return slot == this.slot ? this.attributeModifiers : super.getAttributeModifiers(slot);
     }
 }
